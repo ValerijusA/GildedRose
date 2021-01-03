a5d549b76aa81a8b3a3512bdda4d7ac043dbb9d8
Valerijus Andrejevas <valerijus.a@gmail.com>
2021-01-03 17:00:10 +0200

    Change test_gilded_rose.py

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 
-from gilded_rose import Item, GildedRose
+from python.gilded_rose import Item, GildedRose
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_item():
+    items = [Item("foo", 0, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert ("foo" == items[0].name)
+
+
+def test_items_value_decreases_by_one_everyday():
+    items = [Item("foo", 1, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 0
+
+
+def test_items_value_does_not_drop_below_zero():
+    items = [Item("foo", 0, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 0
+
+
